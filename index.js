96b93c93200645055091bd373ebf250a45358606
Adrian Lynch <adrian.lynch@concrete.cc>
2015-02-17 22:44:59 +0000

    Objects

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect
 
 // Look for a value in an array
-expect([1,2,3]).to.include(2);
-// expect([1,2,3]).to.include(4); // Fail
+expect([1,2,3]).to.include(2)
+// expect([1,2,3]).to.include(4) // Fail
 
-// To compare arrays
-expect([1,2,3]).to.deep.equal([1,2,3]);      // Pass
-// expect([1,2,3,4]).to.deep.equal([1,2,3]); // Fail
-// expect([1,2,3]).to.deep.equal([1,2,3,4]); // Fail
-// expect([1,2,3]).to.equal([1,2,3]);        // Fail
+// Compare array values
+expect([1,2,3]).to.deep.equal([1,2,3])
+// expect([1,2,3,4]).to.deep.equal([1,2,3]) // Fail
+// expect([1,2,3]).to.deep.equal([1,2,3,4]) // Fail
+// expect([1,2,3]).to.equal([1,2,3])        // Fail
 
-// To compare arrays ignoring order
-expect([1,2,3]).to.have.members([3,2,1]);      // Pass
-// expect([1,2,3]).to.have.members([1,2,3,4]); // Fail
-// expect([1,2,3]).to.have.members([1]);       // Fail
+// Compare array values ignoring order
+expect([1,2,3]).to.have.members([3,2,1])
+// expect([1,2,3]).to.have.members([1,2,3,4]) // Fail
+// expect([1,2,3]).to.have.members([1])       // Fail
+
+// Look for property in object
+expect({x: 1, y: 2, z: 3}).to.include.key('x')
+// expect({x: 1, y: 2, z: 3}).to.include.key('a') // Fail
+
+// Look for multiple properties
+expect({x: 1, y: 2, z: 3}).to.include.keys(['x', 'y'])
+
+// Compare all properties
+expect({x: 1, y: 2, z: 3}).to.have.keys(['x', 'y', 'z'])
+// expect({x: 1, y: 2, z: 3}).to.have.keys(['x', 'y', 'z', 'a']) // Fail
--- readme.md
+++ readme.md
@@ -6,24 +6,34 @@ TODO: A quick note on what Mocha is and what Chai is.
 
 If you'd rather look at code, drill into index.js. All expects should pass and are generally paired with their opposite using `not`.
 
+What's wrong with the Chai docs? I don't like them! I've never once looked at them without hitting `Cmd+F` to search the page. Consider this a note-taking exercise.
+
 ## Arrays
 
 ### Expect an array to contain a value
 
-`expect([1,2,3]).to.include(2);`
+`expect([1,2,3]).to.include(2)`
 
 Or not:
 
-`expect([1,2,3]).to.not.include(4);`
+`expect([1,2,3]).to.not.include(4)`
 
 ### Expect an array to be the same as another array
 
-`expect([1,2,3]).to.deep.equal([1,2,3]);`
+`expect([1,2,3]).to.deep.equal([1,2,3])`
 
 Or not:
 
-`expect([1,2,3]).to.deep.equal([1,2,3,4]);`
+`expect([1,2,3]).to.deep.equal([1,2,3,4])`
 
 Note that this fails:
 
-`expect([1,2,3]).to.equal([1,2,3]);`
+`expect([1,2,3]).to.equal([1,2,3])`
+
+## Objects
+
+### Expect object to contain a property
+
+`expect({x: 1, y: 2, z: 3}).to.include.key('x')`
+
+expect(response.result).to.have.keys(adminFields)
