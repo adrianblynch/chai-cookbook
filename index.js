aef534c7c617dd146a318dccac683a12cc10b2fd
Adrian Lynch <adrian.lynch@concrete.cc>
2015-02-17 12:51:58 +0000

    Remove mocha code. Add chai dep.

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 var expect = require('chai').expect;
 
-describe('chai', function() {
+// Look for a value in an array
+expect([1,2,3]).to.include(2);
+// expect([1,2,3]).to.include(4); // Fail
 
-	it('xxx', function(done) {
+// To compare arrays
+expect([1,2,3]).to.deep.equal([1,2,3]);      // Pass
+// expect([1,2,3,4]).to.deep.equal([1,2,3]); // Fail
+// expect([1,2,3]).to.deep.equal([1,2,3,4]); // Fail
+// expect([1,2,3]).to.equal([1,2,3]);        // Fail
 
-		// Look for a value in and not in an array
-		expect([1,2,3]).to.include(2);
-		expect([1,2,3]).to.not.include(4);
-
-		// To compare arrays:
-		expect([1,2,3]).to.not.equal([1,2,3]);
-		expect([1,2,3]).to.deep.equal([1,2,3]);
-
-		// To compare arrays ignoring order
-		expect([1,2,3]).to.have.members([3,2,1]);
-		expect([1,2,3]).to.not.have.members([3]); // Needs 2 and 1 to fail
-
-		done();
-
-	});
-
-});
+// To compare arrays ignoring order
+expect([1,2,3]).to.have.members([3,2,1]);      // Pass
+// expect([1,2,3]).to.have.members([1,2,3,4]); // Fail
+// expect([1,2,3]).to.have.members([1]);       // Fail
